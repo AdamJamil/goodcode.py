990b4269e84b97194ddeba43f869142c1bfd379b
Adam Jamil <adam.a1d9c@gmail.com>
2022-05-01 19:02:14 -0400

    Update goodcode.py

--- goodcode.py
+++ goodcode.py
@@ -1,11 +1,19 @@
 modular inverse
-long inv(long a, long b) { return 1 < a ? b - inv(b % a, a) * b / a : 1; }
+ll inv(ll a, ll b) { return 1 < a ? b - inv(b % a, a) * b / a : 1; }
 
 gcd
-long gcd(long a, long b) { return a == 0 ? b : gcd(b % a, a); }
+ll gcd(ll a, ll b) { return a == 0 ? b : gcd(b % a, a); }
 
 pow mod
-long powmod(long x, long pow) { if (pow == 0) return 1L;long temp = powmod(x,pow / 2);if (pow % 2 == 0) return (temp * temp) % MOD;return (((x * temp) % MOD) * temp) % MOD; }
+ll powmod(ll x, ll pow) { if (pow == 0) return 1ll; ll temp = powmod(x,pow / 2); if (pow % 2 == 0) return (temp * temp) % MOD; return (((x * temp) % MOD) * temp) % MOD; }
 
 lcm = ab/gcd(a,b)
 
+sqrt factor
+map<ll, ll> factor(ll x) {
+    map<ll, ll> ret;
+    for (ll p = 2; p * p <= x; ++p)
+        while (x % p == 0) { x /= p; ret[p]++; }
+    if (x != 1) ret[x]++;
+    return ret;
+}
